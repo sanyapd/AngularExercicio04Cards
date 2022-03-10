0950b6f25b7cfc662d906344a099544d7dabce1c
sanyapd <sanyapdias@gmail.com>
2022-03-10 20:45:05 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { CardComponent } from './card/card.component';
+import { ToggleComponent } from './toggle/toggle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    ToggleComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
